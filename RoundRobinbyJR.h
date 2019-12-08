b96ab61e0730a6e0c8269f93aaf9d42a32891a43
IoT-PLA-9 <58589893+IoT-PLA-9@users.noreply.github.com>
2019-12-08 18:03:59 +0100

    abr_mkr1000 v1

---
+++ RoundRobinbyJR.h
@@ -0,0 +1,19 @@
+  
+
+String ReadLine(char* filename, int x = 1);
+
+void RemoveOldLogs (char* filename, int trgr, int x);
+
+void SaveLogs (char* filename, int trgr);
+
+void PrintFile(char* filename);
+
+int NumberOfLogs(char* filename);
+
+void CopyFile(char* filename1, char* filename2);
+
+void WriteLine(char* filename, String line);
+
+void EraseLastLog (char* filename);
+
+void EraseFirstLog (char* filename, int nlogs);
