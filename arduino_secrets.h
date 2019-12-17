4603b43ac3af25d4f73ab5545e5ec5d63f7650eb
IoT-PLA-9 <58589893+IoT-PLA-9@users.noreply.github.com>
2019-12-17 09:11:48 +0100

    Last One

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,10 +1,10 @@
-#define SECRET_SSID "Casa"
-#define SECRET_PASS "0a1b2c3d4e"
-#define SECRET_MQTT_CLIENT_ID "MKR1000"
-#define SECRET_MQTT_HOST "192.168.2.126"
-#define SECRET_MQTT_USER "mkr1000"
-#define SECRET_MQTT_PASS "mqtt"
-#define SSL_PORT 1883
+#define SECRET_SSID "SSSID_NAME"
+#define SECRET_PASS "SSIDPWD"
+#define SECRET_MQTT_CLIENT_ID "Client_ID"
+#define SECRET_MQTT_HOST "HOST_IP"
+#define SECRET_MQTT_USER "user"
+#define SECRET_MQTT_PASS "pwd"
+#define SSL_PORT 8883
 #define RELAY1 1
 #define RETAIN true
  
